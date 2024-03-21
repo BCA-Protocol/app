77c8b65191f65b6f4c94d763d6c155bc018c537a
Teddy <teodosp@gmail.com>
2024-03-20 22:53:20 -0400

    chart changes

--- src/components/ReferalChart.jsx
+++ src/components/ReferalChart.jsx
@@ -11,13 +11,16 @@ const ApexChart = () => {
       {
         name: "Total",
         data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+        color: "#a21caf"
       },
       {
         name: "Referrals",
         data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+        color: "#ffcc00"
       },
     ],
     options: {
+      background: "#000",
       chart: {
         height: 350,
         type: "line",
@@ -29,19 +32,48 @@ const ApexChart = () => {
         enabled: false,
       },
       stroke: {
-        curve: "straight",
+        curve: "smooth",
       },
       title: {
         text: "Earnings",
         align: "left",
+        style: {
+          color: "#a21caf",
+        }
       },
       grid: {
-        row: {
-          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-          opacity: 0.5,
+        show: false
+      },
+      legend: {
+        labels: {
+          colors: ['#a21caf', '#ffcc00']
+        }
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: '#a21caf'
+          }
+        },
+        axisBorder: {
+          color: '#a21caf'
+        },
+        axisTicks: {
+          color: '#a21caf'
         },
       },
       xaxis: {
+        labels: {
+          style: {
+            colors: '#a21caf'
+          }
+        },
+        axisBorder: {
+          color: '#a21caf'
+        },
+        axisTicks: {
+          color: '#a21caf'
+        },
         categories: [
           "Jan",
           "Feb",
@@ -62,7 +94,7 @@ const ApexChart = () => {
       <div id="chart">
         {/* {typeof window !== "undefined" && ( */}
         <ReactApexCharts
-          className="px-2 py-6 bg-gradient-to-l from-purple-900 to-black rounded-xl"
+          className="py-6 bg-black rounded-xl"
           options={chartData.options}
           series={chartData.series}
           type="line"
