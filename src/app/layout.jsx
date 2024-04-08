851d052ebd1e1639d5cdc87e3bd0b653e26a3a30
Teddy <teodosp@gmail.com>
2024-04-08 02:56:43 -0400

    added google codes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import "./globals.css";
 import { Open_Sans, Roboto_Mono } from "next/font/google";
 import clsx from "clsx";
+import Script from "next/script"; // Import Script component
 
 const open_sans = Open_Sans({
   subsets: ["latin"],
@@ -40,8 +41,30 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180I hax180" href="/favicon.png" />
         <meta name="theme-color" content="#ffffff" />
+        {/* GTM Script */}
+        <Script strategy="afterInteractive">
+          {(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != "dataLayer" ? "&l=" + l : "";
+            j.async = true;
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, "script", "dataLayer", "GTM-WVQ3KSLP")}
+        </Script>
       </head>
       <body className="flex flex-col h-full bg-black">
+        {/* GTM noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WVQ3KSLP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {!isPublicPage && (
           <>
             <Header />
