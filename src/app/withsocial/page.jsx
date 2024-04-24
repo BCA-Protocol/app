37ece24dac913d186c36f708340d487804dfa5a4
Teddy <teodosp@gmail.com>
2024-04-23 20:34:09 -0400

    added telegram, discord and twitter providers

---
+++ src/app/withsocial/page.jsx
@@ -0,0 +1,76 @@
+"use client";
+// import DiscorOauth
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { IconFidgetSpinner } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
+import { handleTaskCompletion } from "@/utils/utils";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+const Home = () => {
+  const searchParams = useSearchParams();
+  const socialCode = searchParams.get("code");
+  const socialType = searchParams.get("type");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (socialCode !== null && socialType) {
+      setLoading(true);
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const uid = user.uid;
+          const gettoken = async (code) => {
+            if (socialType == "discord") {
+              const userData = await fetch(
+                `${window.location.origin}/api/discorduser?code=${code}`
+              ).then((res) => res.json());
+              if (userData) {
+                const taskRes = await handleTaskCompletion(
+                  uid,
+                  "connectDiscord",
+                  {
+                    discordData: userData.userData,
+                  }
+                );
+                router.replace("/");
+              }
+            } else if (socialType == "twitter") {
+              const userData = await fetch(
+                `${window.location.origin}/api/twitteruser?code=${code}`
+              ).then((res) => res.json());
+              if (userData) {
+                const taskRes = await handleTaskCompletion(
+                  uid,
+                  "connectTwitter",
+                  {
+                    twitterData: userData.userData,
+                  }
+                );
+                router.replace("/");
+              }
+            }
+          };
+
+          gettoken(socialCode);
+        } else {
+          console.log("Not Redirect");
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      {loading && (
+        <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-gray-500 bg-opacity-50">
+          <IconFidgetSpinner className="w-20 h-20 animate-spin" />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
