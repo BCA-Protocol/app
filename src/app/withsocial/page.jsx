faeb3b263b7fe2f170b915f47abe6899df47cd41
Ahmad Humayun <ahmad.humayun@adara.com>
2024-04-26 21:02:19 +0500

    Discord And Twitter Error Handling

--- src/app/withsocial/page.jsx
+++ src/app/withsocial/page.jsx
@@ -6,6 +6,7 @@ import { IconFidgetSpinner } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
 import { handleTaskCompletion } from "@/utils/utils";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Notification from "@/components/notifications/notification";
 
 const auth = getAuth();
 const Home = () => {
@@ -13,6 +14,8 @@ const Home = () => {
   const socialCode = searchParams.get("code");
   const socialType = searchParams.get("type");
   const [loading, setLoading] = useState(false);
+  const [errorSocial, setErrorSocial] = useState("");
+  const [notification, setNotification] = useState(null);
 
   const router = useRouter();
 
@@ -24,33 +27,49 @@ const Home = () => {
           const uid = user.uid;
           const gettoken = async (code) => {
             if (socialType == "discord") {
-              const userData = await fetch(
+              const discordResponse = await fetch(
                 `${window.location.origin}/api/discorduser?code=${code}`
               ).then((res) => res.json());
-              if (userData) {
+              if(discordResponse.error){
+                setLoading(false);
+                setNotification({
+                  type: "error",
+                  message: "Couldn't Connect To Discord: " + discordResponse.error,
+                  show: true,
+                });
+              }
+              if (discordResponse.userData) {
                 const taskRes = await handleTaskCompletion(
                   uid,
                   "connectDiscord",
                   {
-                    discordData: userData.userData,
+                    discordData: discordResponse.userData,
                   }
                 );
-                router.replace("/");
               }
+              router.replace(errorSocial?"/?socialerror=discord":"/");
             } else if (socialType == "twitter") {
-              const userData = await fetch(
+              const twitterResponse = await fetch(
                 `${window.location.origin}/api/twitteruser?code=${code}`
               ).then((res) => res.json());
-              if (userData) {
+              if(twitterResponse.error){
+                setLoading(false);
+                setNotification({
+                  type: "error",
+                  message: "Couldn't Connect To Twitter: " + twitterResponse.error,
+                  show: true,
+                });
+              }
+              if (twitterResponse.userData) {
                 const taskRes = await handleTaskCompletion(
                   uid,
                   "connectTwitter",
                   {
-                    twitterData: userData.userData,
+                    twitterData: twitterResponse.userData,
                   }
                 );
-                router.replace("/");
               }
+              router.replace("/");
             }
           };
 
@@ -68,6 +87,9 @@ const Home = () => {
         <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-gray-500 bg-opacity-50">
           <IconFidgetSpinner className="w-20 h-20 animate-spin" />
         </div>
+      )}
+      {notification && notification.show && (
+        <Notification message={notification.message} type={notification.type} />
       )}
     </>
   );
