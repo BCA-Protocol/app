a425157d755f1c001af73dbdb424a0a2201ae8e8
supun19 <supun.12@cse.mrt.ac.lk>
2024-05-05 17:55:29 +0530

    check refernece

--- supabase/functions/checkReference/index.ts
+++ supabase/functions/checkReference/index.ts
@@ -16,7 +16,7 @@ Deno.serve(async (req) => {
     Deno.env.get('SUPABASE_ANON_KEY') ?? '',
     { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
   )
-  const { data, error }  = await supabase.from('users').select().eq('id', referenceId).single();
+  const { data, error }  = await supabase.from('users').select().eq('id', referenceId).maybeSingle();
 
   if (error) {
     throw error
@@ -38,9 +38,8 @@ Deno.serve(async (req) => {
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/checkReference' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+  curl -i --location --request POST 'https://onmkwytrhfghwfilzbrt.supabase.co/functions/v1/checkReference' \
     --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
+    --data '{"referenceId":"6ae71fb3-7358-4b34-ac81-0a9723df0318"}'
 
 */
